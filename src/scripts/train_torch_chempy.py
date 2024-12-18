9de918c95f4df66db061bad2ddd5b6ced8246c23
showyourwork <showyourwork>
2024-12-18 13:24:00 +0100

    [showyourwork] overleaf sync

--- src/scripts/train_torch_chempy.py
+++ src/scripts/train_torch_chempy.py
@@ -57,9 +57,9 @@ priors = torch.tensor([[a.priors[opt][0], a.priors[opt][1]] for opt in a.to_opti
 class Model_Torch(torch.nn.Module):
     def __init__(self):
         super(Model_Torch, self).__init__()
-        self.l1 = torch.nn.Linear(train_x.shape[1], 100)
+        self.l1 = torch.nn.Linear(val_x.shape[1], 100)
         self.l2 = torch.nn.Linear(100, 40)
-        self.l3 = torch.nn.Linear(40, train_y.shape[1])
+        self.l3 = torch.nn.Linear(40, val_y.shape[1])
 
     def forward(self, x):
         x = torch.tanh(self.l1(x))
