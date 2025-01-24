9de229e7fcdc5805a77887321bc4c9f67ce07519
showyourwork <showyourwork>
2025-01-24 09:42:01 +0100

    [showyourwork] overleaf sync

--- src/scripts/tng_inference.py
+++ src/scripts/tng_inference.py
@@ -41,42 +41,34 @@ def add_noise(true_abundances):
     return obs_abundances
 
 # ----- Load the data -------------------------------------------------------------------------------------------------------------------------------------------
-# CHEMPY data with alternative yields
-data_alternative_yields = np.load(paths.data / "chempy_data/chempy_alternative_yields.npz", mmap_mode='r')
+# TNG simulation data
+data_tng = np.load("data/chempy_data_paper/Mock_Data_TNG.npz", mmap_mode='r')
 
-elements = data_alternative_yields['elements']
-alt_x = data_alternative_yields['params']
-alt_y = data_alternative_yields['abundances']
+tng_y = data_tng['true_abuns']
 
-alt_x, alt_y = clean_data(alt_x, alt_y)
-
-# Remove H from data, because it is just used for normalization (output with index 2)
-elements = np.delete(elements, 2)
-alt_y = np.delete(alt_y, 2, 1)
-
-alt_y_obs = add_noise(alt_y)
+tng_y_obs = add_noise(tng_y)
 
 # ----- Load posterior -----
 with open(paths.data / f'posterior_{name}.pickle', 'rb') as f:
     posterior = pickle.load(f)
 
 # ----- Evaluate the posterior -------------------------------------------------------------------------------------------------------------------------------------------
-alpha_IMF_alt_obs = []
-log10_N_Ia_alt_obs = []
+alpha_IMF_tng_obs = []
+log10_N_Ia_tng_obs = []
 simulations = 1000
-N_stars = len(alt_y_obs)
+N_stars = len(tng_y_obs)
 
 start = t.time()
-for i in tqdm(range(len(alt_y_obs))):
-    x = add_noise(alt_y_obs[i])
+for i in tqdm(range(len(tng_y_obs))):
+    x = add_noise(tng_y_obs[i])
     alpha, N_Ia = posterior.sample((simulations,), x=x, show_progress_bars=False)[:,0:2].T
-    alpha_IMF_alt_obs.append(alpha)
-    log10_N_Ia_alt_obs.append(N_Ia)
+    alpha_IMF_tng_obs.append(alpha)
+    log10_N_Ia_tng_obs.append(N_Ia)
 end = t.time()
 print(f'Time to run {simulations} simulations for {N_stars} stars: {end-start:.3f} s')
 
-alpha_IMF_alt_obs = np.array(alpha_IMF_alt_obs)
-log10_N_Ia_alt_obs = np.array(log10_N_Ia_alt_obs)
+alpha_IMF_tng_obs = np.array(alpha_IMF_tng_obs)
+log10_N_Ia_tng_obs = np.array(log10_N_Ia_tng_obs)
 
 
 # ------ plot the data -------------------------------------------------------------------------------------------------------------------------------------------
@@ -84,11 +76,11 @@ log10_N_Ia_alt_obs = np.array(log10_N_Ia_alt_obs)
 # Philcox&Rybizki 2019 Table 3
 philcox = {}
 philcox["n_stars"] = np.array([1,10,100])
-philcox["med"] = np.array([[-2.25,-3.01],[-2.21,-2.96],[-2.22,-2.96]])
-philcox["up"] = np.array([[-2.14,-2.86],[-2.17,-2.88],[-2.20,-2.93]])
-philcox["lo"] = np.array([[-2.34,-3.16],[-2.25,-3.04],[-2.24,-2.98]])
+philcox["med"] = np.array([[-2.27,-2.86],[-2.27,-2.87],[-2.28,-2.89]])
+philcox["up"] = np.array([[-2.19,-2.75],[-2.24,-2.84],[-2.27,-2.88]])
+philcox["lo"] = np.array([[-2.35,-3.97],[-2.3,-2.91],[-2.29,-2.90]])
 
-stars = np.arange(1,len(alt_y_obs))
-n_stars_plot_comp2(alpha_IMF_alt_obs, log10_N_Ia_alt_obs, global_params, philcox, "CHEMPY_alternative_yields_Nstar_comp", stars)
+stars = np.arange(1,len(tng_y_obs))
+n_stars_plot_comp2(alpha_IMF_tng_obs, log10_N_Ia_tng_obs, global_params, philcox, "TNG_sim_Nstar_comp", stars)
 
-gaussian_posterior_plot(alpha_IMF_alt_obs, log10_N_Ia_alt_obs, global_params, title="CHEMPY_alternative_yields")
+gaussian_posterior_plot(alpha_IMF_tng_obs, log10_N_Ia_tng_obs, global_params, title="TNG_simulation")
