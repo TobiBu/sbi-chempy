d31cd2397cac08150e2438196bcd5fa7ba267686
showyourwork <showyourwork>
2025-02-18 13:57:21 +0100

    [showyourwork] overleaf sync

--- src/scripts/train_torch_chempy.py
+++ src/scripts/train_torch_chempy.py
@@ -14,7 +14,7 @@ import time as t
 import schedulefree
 
 # ---  You want to retrain? ---
-re_train = True #False
+re_train = False
 
 # ----- Load the data ---------------------------------------------------------------------------------------------------------------------------------------------
 # ---  Load in the validation data ---
@@ -144,41 +144,41 @@ if re_train:
     plt.savefig(paths.figures / "loss_NN_simulator.pdf")
     plt.clf()
 
-else:
-    model.load_state_dict(torch.load(paths.data / 'pytorch_state_dict.pt'))
-    print("Model loaded")
 
-    # ----- Calculate the Absolute Percantage Error -----
+model.load_state_dict(torch.load(paths.data / 'pytorch_state_dict.pt'))
+print("Model loaded")
 
-    ape = 100 * torch.abs((val_y - model(val_x)) / val_y).detach().numpy()
+# ----- Calculate the Absolute Percantage Error -----
 
-    fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.20, .80)})
+ape = 100 * torch.abs((val_y - model(val_x)) / val_y).detach().numpy()
 
-    ax_hist.hist(ape.flatten(), bins=100, density=True, cumulative=True, range=(0, 30), color='tomato')
-    ax_hist.hist(ape.flatten(), bins=100, density=True, range=(0, 30), color='tomato')
-    ax_hist.set_xlabel('Error (%)', fontsize=15)
-    ax_hist.set_ylabel('CDF', fontsize=15)
-    ax_hist.spines['top'].set_visible(False)
-    ax_hist.spines['right'].set_visible(False)
-    # percentiles
-    p1,p2,p3 = np.percentile(ape, [25, 50, 75])
-    ax_hist.axvline(p2, color='black', linestyle='--')
-    ax_hist.axvline(p1, color='black', linestyle='dotted')
-    ax_hist.axvline(p3, color='black', linestyle='dotted')
-    ax_hist.text(p2, 0.2, fr'${p2:.1f}^{{+{p3-p2:.1f}}}_{{-{p2-p1:.1f}}}\%$', fontsize=12, verticalalignment='top')
+fig, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.20, .80)})
 
-    ax_box.boxplot(ape.flatten(), vert=False, autorange=False, widths=0.5, patch_artist=True, showfliers=False, boxprops=dict(facecolor='tomato'), medianprops=dict(color='black'))
-    ax_box.set(yticks=[])
-    ax_box.spines['left'].set_visible(False)
-    ax_box.spines['right'].set_visible(False)
-    ax_box.spines['top'].set_visible(False)
+ax_hist.hist(ape.flatten(), bins=100, density=True, cumulative=True, range=(0, 30), color='tomato')
+ax_hist.hist(ape.flatten(), bins=100, density=True, range=(0, 30), color='tomato')
+ax_hist.set_xlabel('Error (%)', fontsize=15)
+ax_hist.set_ylabel('CDF', fontsize=15)
+ax_hist.spines['top'].set_visible(False)
+ax_hist.spines['right'].set_visible(False)
+# percentiles
+p1,p2,p3 = np.percentile(ape, [25, 50, 75])
+ax_hist.axvline(p2, color='black', linestyle='--')
+ax_hist.axvline(p1, color='black', linestyle='dotted')
+ax_hist.axvline(p3, color='black', linestyle='dotted')
+ax_hist.text(p2, 0.2, fr'${p2:.1f}^{{+{p3-p2:.1f}}}_{{-{p2-p1:.1f}}}\%$', fontsize=12, verticalalignment='top')
 
-    #fig.suptitle('APE of the Neural Network', fontsize=20)
-    plt.xlim(0, 30)
-    fig.tight_layout()
+ax_box.boxplot(ape.flatten(), vert=False, autorange=False, widths=0.5, patch_artist=True, showfliers=False, boxprops=dict(facecolor='tomato'), medianprops=dict(color='black'))
+ax_box.set(yticks=[])
+ax_box.spines['left'].set_visible(False)
+ax_box.spines['right'].set_visible(False)
+ax_box.spines['top'].set_visible(False)
 
-    plt.savefig(paths.figures / "ape_NN.pdf")
-    plt.clf()
+#fig.suptitle('APE of the Neural Network', fontsize=20)
+plt.xlim(0, 30)
+fig.tight_layout()
 
-    with open(paths.output / 'ape_NN.txt', 'w') as f:
-        f.write(f'${p2:.1f}^{{+{p3-p2:.1f}}}_{{-{p2-p1:.1f}}}\,\%$%')
+plt.savefig(paths.figures / "ape_NN.pdf")
+plt.clf()
+
+with open(paths.output / 'ape_NN.txt', 'w') as f:
+    f.write(f'${p2:.1f}^{{+{p3-p2:.1f}}}_{{-{p2-p1:.1f}}}\,\%$%')
