8c186d2a97755358531cfcd77936aeb285b847e2
showyourwork <showyourwork>
2025-02-25 23:26:00 +0100

    [showyourwork] overleaf sync

--- src/scripts/plot_functions.py
+++ src/scripts/plot_functions.py
@@ -588,7 +588,7 @@ def gaussian_posterior_plot_n_stars(alpha_IMF, log10_N_Ia, global_params, title,
     sigma = np.array([3,2,1], dtype=float)
     for n in sigma:
         levels.append(posterior.pdf([philcox['med'][:,0][-1]+n*sigma_philcox_1[-1], philcox['med'][:,1][-1]+n*sigma_philcox_2[-1]**2]))
-    CS = plt.contourf(x, y, posterior.pdf(pos), levels=levels, colors='gray', linestyles='dotted')
+    CS = plt.contour(x, y, posterior.pdf(pos), levels=levels, colors='gray', linestyles='dotted')
     #text = plt.clabel(CS, inline=True, fontsize=25)
     #for t in text:
     #    i = np.abs(np.array(levels) - float(t._text)).argmin()
