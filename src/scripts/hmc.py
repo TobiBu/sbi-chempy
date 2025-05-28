b62046a969d58b81ee384b9c301defa5dd7cd55d
showyourwork <showyourwork>
2025-05-28 23:10:58 +0200

    [showyourwork] overleaf sync

--- src/scripts/hmc.py
+++ src/scripts/hmc.py
@@ -41,9 +41,9 @@ def clean_data(x, y):
 
 
 val_theta, val_x = clean_data(val_theta, val_x)
-obs_abundances = val_x[:100]
-val_x = val_x[:100]
-val_theta = val_theta[:100]
+obs_abundances = val_x[:200]
+val_x = val_x[:200]
+val_theta = val_theta[:200]
 obs_errors = np.ones_like(obs_abundances) * 0.05  # fixed Gaussian noise
 
 
